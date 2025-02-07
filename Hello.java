322542796b44b219cd44b306dc10e583183d7c81
rafeekf7 <rafeekjr7@gmail.com>
2025-02-06 23:23:11 -0500

    test2

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 public class Hello {
-    public static void main(String[]args){
+    public static void main(String[] args){
         for(int i = 1; i<=10; i++){
-            System.out.println("Hello World..." + i);
+            System.out.println("Hello World..." +i);
             System.out.println("New Change");
             System.out.println("New Change 2");
             System.out.println("New Change 3");
         }
     }
 }
+
